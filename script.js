7c275bcf638e0b93da42a3f5c82d3c5a1ac657cf
MathewsBaio <baiomathews@gmail.com>
2024-04-01 20:10:50 -0300

    c

--- script.js
+++ script.js
@@ -1,152 +1,115 @@
-let table = document.querySelectorAll("td");
-let play = 0;
-let position = 0;
-let zero, one, two, three, four, five, six,  seven, eight;
-//let houseArray = [zero, one, two, three, four, five, six,  seven, eight]; 
-let houseArray = Array.from(table);
+let campos = document.querySelectorAll('td');
+let jogada = 1;
+let posicao = 0;
+let tabuleiro = [0,0,0,
+                 0,0,0,
+                 0,0,0];
+let ativo = true;
+let texto = document.createElement('h1');
+document.body.appendChild(texto);
+let restart = document.createElement("button")
+restart.innerText = "Reiniciar Jogo"
 
-table.forEach(house =>{
-    house.setAttribute("id", position++);
-})
 
-position= 0;
-
-table.forEach(house =>{
+campos.forEach( casa => {
+    casa.setAttribute("posicao",posicao++);
     
-    house.addEventListener("click", event =>{
-        
-        if(event.target.innerText == ""){
-            if(play % 2 == 0) {
-                event.target.innerText = "O";
-                houseArray[house] = "O"
-                
-                console.log(houseArray);
-            } else {
-                event.target.innerText = "X";
-                houseArray[house] = "X"
-                
-                console.log(houseArray);
+    casa.addEventListener("click", event =>{
+        if (ativo){
+            let clicado = event.target;
+            let posicao = clicado.getAttribute('posicao');
+            let atual = 0;
+            if (clicado.innerText == ''){
+                if (jogada % 2 == 1){
+                    atual = "O";
+                }else{
+                    atual = "X";
+                }
+                clicado.innerText = atual;
+                tabuleiro[posicao] = atual;
+                jogada++;
+                let ganhador = verificaVitoria()
+                if (ganhador){
+                    texto.innerText = `O jogador ${ganhador} venceu!`;
+                    ativo = false;
+                    document.body.appendChild(restart)
+                    restart.addEventListener("click", event=>{
+                        restartGame();
+                    })
+
+                } else if(jogada == 10){
+                    texto.innerText = "Deu velha!"
+                    document.body.appendChild(restart)
+                    restart.addEventListener("click", event=>{
+                        restartGame();
+                    })
+                }
+            }else{
+                alert("Jogada invalida!");
             }
-            play++;
-            verifyWinner(play)
-        } else{
-            alert("Invalid move");
-        }
+        }        
+
+    });
+});
+
+
+function verificaVitoria(){
+    let ganhador = null;
+    if (((tabuleiro[0] == tabuleiro[1]) && //linha 1
+         (tabuleiro[1] == tabuleiro[2]) &&
+         (tabuleiro[0] != 0) &&
+         (ganhador = tabuleiro[0])) || 
+         
+        ((tabuleiro[3] == tabuleiro[4]) && //linha 2
+         (tabuleiro[4] == tabuleiro[5]) &&
+         (tabuleiro[3] != 0) &&
+         (ganhador = tabuleiro[3])) || 
+          
+        ((tabuleiro[6] == tabuleiro[7]) && //linha 3
+         (tabuleiro[7] == tabuleiro[8]) &&
+         (tabuleiro[6] != 0) &&
+         (ganhador = tabuleiro[6])) || 
+          
+        ((tabuleiro[0] == tabuleiro[3]) && //coluna 1
+        (tabuleiro[3] == tabuleiro[6]) &&
+        (tabuleiro[0] != 0) &&
+        (ganhador = tabuleiro[0])) || 
         
-    })
-    
-})
-
-
-function verifyWinner(play) {
-    let someoneWined = false;
-
-    if(table[0].innerText == "O" && table[1].innerText== "O" && table[2].innerText == "O") {
-        alert("Player one wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[3].innerText == "O" && table[4].innerText== "O" && table[5].innerText == "O") {
-        alert("Player one wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[6].innerText == "O" && table[7].innerText== "O" && table[8].innerText == "O") {
-        alert("Player one wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[0].innerText == "O" && table[3].innerText== "O" && table[6].innerText == "O") {
-        alert("Player one wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[1].innerText == "O" && table[4].innerText== "O" && table[7].innerText == "O") {
-        alert("Player one wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[2].innerText == "O" && table[5].innerText== "O" && table[8].innerText == "O") {
-        alert("Player one wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[2].innerText == "O" && table[4].innerText== "O" && table[6].innerText == "O") {
-        alert("Player one wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[0].innerText == "O" && table[4].innerText== "O" && table[8].innerText == "O") {
-        alert("Player one wins!");
-        restartGame();
-        someoneWined = true;
-    } 
-
-    if(table[0].innerText == "X" && table[1].innerText== "X" && table[2].innerText == "X") {
-        alert("Player two wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[3].innerText == "X" && table[4].innerText== "X" && table[5].innerText == "X") {
-        alert("Player two wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[6].innerText == "X" && table[7].innerText== "X" && table[8].innerText == "X") {
-        alert("Player two wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[0].innerText == "X" && table[3].innerText== "X" && table[6].innerText == "X") {
-        alert("Player two wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[1].innerText == "X" && table[4].innerText== "X" && table[7].innerText == "X") {
-        alert("Player two wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[2].innerText == "X" && table[5].innerText== "X" && table[8].innerText == "X") {
-        alert("Player two wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[2].innerText == "X" && table[4].innerText== "X" && table[6].innerText == "X") {
-        alert("Player two wins!");
-        restartGame();
-        someoneWined = true;
-    } else if (table[0].innerText == "X" && table[4].innerText== "X" && table[8].innerText == "X") {
-        alert("Player two wins!");
-        restartGame();
-        someoneWined = true;
+        ((tabuleiro[1] == tabuleiro[4]) && //coluna 2
+        (tabuleiro[4] == tabuleiro[7]) &&
+        (tabuleiro[1] != 0) &&
+        (ganhador = tabuleiro[1])) || 
+          
+        ((tabuleiro[2] == tabuleiro[5]) && //coluna 3
+        (tabuleiro[5] == tabuleiro[8]) &&
+        (tabuleiro[2] != 0) &&
+        (ganhador = tabuleiro[2])) || 
+        
+        ((tabuleiro[0] == tabuleiro[4]) && //diagonal 1
+        (tabuleiro[4] == tabuleiro[8]) &&
+        (tabuleiro[0] != 0) &&
+        (ganhador = tabuleiro[0])) || 
+        
+        ((tabuleiro[2] == tabuleiro[4]) && //diagonal 2
+        (tabuleiro[4] == tabuleiro[6]) &&
+        (tabuleiro[2] != 0) &&
+        (ganhador = tabuleiro[2])))
+    {
+        return ganhador;
     }
 
-    if (play == 9 && !someoneWined){
-        alert("Empate!");
-        restartGame();
-    }
-
+    return ganhador;
 }
-
 
 function restartGame() {
-
-    table.forEach(house =>{
-        house.innerText = "";
-        play = 0;
+    texto.innerText = ""
+    campos.forEach(house =>{
+        house.innerText = ""
     })
-
+    jogada = 1
+    ativo = true
+    tabuleiro = [0,0,0,
+                0,0,0,
+                0,0,0];
+    document.body.removeChild(restart)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-for (let i = 0; i < houseArray.length; i++) {
-    if (houseArray[i] == "") {
-        houseArray[i] = "O";
-        console.log(houseArray);
-        break;
-    }
-} */
